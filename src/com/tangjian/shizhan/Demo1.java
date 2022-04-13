eac45e9c79fab70b8321881e991ed25686a6489f
TJ <975751290@qq.com>
2022-04-13 20:02:40 +0800

    JVM实战篇

---
+++ src/com/tangjian/shizhan/Demo1.java
@@ -0,0 +1,16 @@
+package com.tangjian.shizhan;
+
+/**
+ * @Author tang
+ * @Date 2022/4/9 1:37
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        byte[] array1 = new byte[1024 * 1024];
+        array1 = new byte[1024 * 1024];
+        array1 = new byte[1024 * 1024];
+        array1 = null;
+
+        byte[] array2 = new byte[2 * 1024 * 1024];
+    }
+}
